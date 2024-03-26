2cfd7a7d41e0cb34db648de5e3ec8212c9ffaf2b
lu <lucrezia.penati@gmail.com>
2024-03-26 15:44:17 +0100

    updated rps computation

--- rps_generator.py
+++ rps_generator.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from typing import Union, Optional, List, Dict, Tuple, TypeVar
+from typing import Optional, List, Dict, Tuple
 import sys
+import math
 
 
 ############################ read_dataset ####################################
@@ -43,23 +44,19 @@ def get_abund_data(dataset: pd.DataFrame, cell_line_index:int) -> Optional[pd.Se
                            Non-numeric or missing (NaN) values are converted to zero.
                            Returns None if the cell index is invalid.
     """
-    
     if cell_line_index < 0 or cell_line_index >= len(dataset.index):
         print(f"Errore: L'indice della cell line '{cell_line_index}' non è valido.")
         return None
 
     cell_line_name = dataset.iloc[cell_line_index, 0] 
     abundances_series = dataset.iloc[cell_line_index, 1:]  
-
-    # Converti i valori non numerici o mancanti (NaN) a zero
-    abundances_series = pd.to_numeric(abundances_series, errors='coerce').fillna(0)
     abundances_series.name = cell_line_name
 
     return abundances_series 
 
 
 ############################ update_metabolite_names ####################################
-def update_metabolite_names(abundances_series: pd.Series, syn_dict: Dict[str, List[str]]) -> Optional[pd.Series]:
+def update_metabolite_names(abundances_dict: Dict[str, float], syn_dict: Dict[str, List[str]]) -> Dict[str, float]:
     """
     Update metabolite names in the abundance series based on synonyms provided in the synonym dictionary.
 
@@ -68,162 +65,99 @@ def update_metabolite_names(abundances_series: pd.Series, syn_dict: Dict[str, Li
         syn_dict (dict): A dictionary where keys are general metabolite names and values are lists of possible synonyms.
 
     Returns:
-        pd.Series or None : An updated series where metabolite names have been replaced with their corresponding general names
-                            according to the synonym dictionary. If a metabolite name doesn't have a synonym or it has already the general name, it remains unchanged.
-                            Returns None if abundances_series is None.
+        dict: An updated series where metabolite names have been replaced with their corresponding general names
+              according to the synonym dictionary. If a metabolite name doesn't have a synonym it is deleted, whereas 
+              if it has already the general name, it remains unchanged.
     """
-    if abundances_series is None:
-        print("Error: abundances_series cannot be None.")
-        return None
-
-    updated_abundances = abundances_series.copy()
-
-    for metabolite, abundance in abundances_series.items():
+    updated_abundances = abundances_dict.copy()
+    for metabolite, abundance in list(abundances_dict.items()):
         for key, synonyms in syn_dict.items():
-            if metabolite in synonyms:
-                updated_abundances = updated_abundances.rename(index={metabolite: key})
+            if metabolite in synonyms or metabolite in key:
+                updated_abundances[key] = updated_abundances.pop(metabolite)
                 break
         else:
-            updated_abundances = updated_abundances.drop(index=metabolite)
+            del updated_abundances[metabolite]
 
     return updated_abundances
 
 
-############################ sort_reactions ####################################
-def sort_reactions(reactions: Dict[str, Dict[str, int]], updated_abundances: pd.Series) -> Optional[Dict[str, Dict[str, int]]]:
+############################ check_missing_metab ####################################
+def check_missing_metab(reactions: Dict[str, Dict[str, int]], updated_abundances: Dict[str, float]) -> Tuple[Dict[str, float], List[str]]:
     """
-    Sort reactions based on the order of metabolites in the updated abundance series.
+    Check for missing metabolites in the abundances dictionary compared to the reactions dictionary and update abundances accordingly.
 
-    Args:
-        reactions (dict): A dictionary containing reactions data.
-                          Keys are metabolite names and values are dictionaries representing stoichiometric coefficients.
-        updated_abundances (pandas.Series): A pandas Series containing updated metabolite abundances.
-                                             Metabolite names in the series will determine the order of reactions.
+    Parameters:
+        reactions (dict): A dictionary representing reactions where keys are reaction names and values are dictionaries containing metabolite names as keys and stoichiometric coefficients as values.
+        updated_abundances (dict): A dictionary representing abundances where keys are metabolite names and values are their corresponding abundances.
 
     Returns:
-        dict or None: An updated dictionary of reactions that are sorted based on the order in which metabolites are in the series.
-                      If the metabolite in a reaction is not found in the updated abundance series, the reaction is not included in the updated dictionary.
-                      Returns None if updated_abundances is None
-
+        tuple[dict, list]: A tuple containing:
+            - A dictionary representing updated abundances where metabolite names have been added or set to 1 if missing in the original abundances dictionary.
+            - A list of metabolite names that were missing in the original abundances dictionary and thus their aboundances were set to 1.
     """
-    if updated_abundances is None:
-       print("Error: abundances_series cannot be None.")
-       return None
+    missing_list=[]
+    for reaction in reactions.values():
+        for metabolite in reaction.keys():
+          if metabolite not in updated_abundances:
+            updated_abundances[metabolite]= 1
+            missing_list.append(metabolite)
 
-    ordered_reactions = {}
-
-    for reaction_id in updated_abundances.index:
-        if reaction_id in reactions:
-            ordered_reactions[reaction_id] = reactions[reaction_id]
-
-    return ordered_reactions
+    return updated_abundances, missing_list
 
 
-############################ extract_coeff_vector ####################################
-def extract_coeff_vector(ordered_reactions: Dict[str, Dict[str, int]], reaction_id: str) -> Optional[List[int]]:
+############################ calculate_rps ####################################
+def calculate_rps(reactions: Dict[str, Dict[str, int]], abundances: Dict[str, float], black_list: List[str], missing_list: List[str]) -> Dict[str, float]:
     """
-    Extract the stoichiometric coefficient vector for a specific reaction index from the ordered reactions.
+    Calculate the Reaction Propensity scores (RPS) based on the availability of reaction substrates, for (ideally) each input model reaction and for each sample.
+    The score is computed as the product of the concentrations of the reacting substances, with each concentration raised to a power equal to its stoichiometric coefficient
+    for each reaction using the provided coefficient and abundance values.
 
-    Args:
-        ordered_reactions (dict): A dictionary containing reaction information.
-                                  Keys are reaction IDs and values are dictionaries with metabolite IDs and coefficients.
-        reaction_id (str): The name of the reaction for which to extract the coefficient vector.
+    Parameters:
+        reactions (dict): A dictionary representing reactions where keys are reaction names and values are dictionaries containing metabolite names as keys and stoichiometric coefficients as values.
+        abundances (dict): A dictionary representing metabolite abundances where keys are metabolite names and values are their corresponding abundances.
+        black_list (list): A list containing metabolite names that should be excluded from the RPS calculation.
+        missing_list (list): A list containing metabolite names that were missing in the original abundances dictionary and thus their values were set to 1.
 
     Returns:
-        list or None: A list containing the coefficient values for the specified reaction index.
-                      Returns None if no reaction is found at the given index.
+        dict: A dictionary containing Reaction Propensity Scores (RPS) where keys are reaction names and values are the corresponding RPS scores.
     """
-    if reaction_id in ordered_reactions:
-        coeff_vector = list(ordered_reactions[reaction_id].values())
-        return coeff_vector
-    else:
-        print(f"No reaction found for '{reaction_id}'.")
-        return None
-
-
-############################ extract_abundance_vector ####################################
-def extract_abundance_vector(updated_abundances: pd.Series) -> Optional[List[float]]:
-    """
-    Extract the abundance vector from an abundance series.
-
-    Args:
-        updated_abundances (pandas.Series): A pandas Series containing abundance values for metabolites.
-
-    Returns:
-        list or None: A list containing the abundance values extracted from the updated abundance series.
-                      Returns None if updated_abundances is None
-    """
-    if updated_abundances is None:
-       print("Error: abundances_series cannot be None.")
-       return None
-
-    return updated_abundances.tolist()
-
-
-############################ rps_calculator ####################################
-def rps_calculator(coeff_vector: List[int], abundances_vector: List[float]) -> Optional[List[float]]:
-    """
-    Calculate the Reaction Propensity scores (RPS) based on the availability of reaction substrates, for (ideally) each input model reaction and for each sample. 
-    The score is computed as the product of the concentrations of the reacting substances, with each concentration raised to a power equal to its stoichiometric coefficient 
-    for each reaction using the provided coefficient and abundance vectors.
-
-    Args:
-        coeff_vector (list): A list containing the coefficient values for each reaction.
-        abundances_vector (list): A list containing the abundance values for each metabolite.
-
-    Returns:
-        list or None: A list containing the RPS scores calculated for each reaction.
-                      Returns None if either the coefficient vector or the abundance vector is None.
-
-    """
-    if coeff_vector is None or abundances_vector is None:
-        print("Error: input vectors cannot be None.")
-        return None
-    
-    rps_scores = []
-
-    for coeff, abundance in zip(coeff_vector, abundances_vector):
-        if abundance == 0:
-            rps_scores.append(0)
-        else:
-            rps_score = np.prod(abundance ** coeff)
-            rps_scores.append(rps_score)
+    rps_scores = {}
+ 
+    for reaction_name, substrates in reactions.items():
+        total_contribution = 0
+        metab_significant = False
+        for metabolite, stoichiometry in substrates.items():
+            temp = 1 if math.isnan(abundances[metabolite]) else abundances[metabolite]
+            if metabolite not in black_list and metabolite not in missing_list:
+              metab_significant = True
+            total_contribution += temp * stoichiometry
+        
+        rps_scores[reaction_name] = total_contribution if metab_significant else math.nan
     
     return rps_scores
 
 
-
-############################ calculate_rps_for_reactions ####################################
-def calculate_rps_for_reactions(reactions: Dict[str, Dict[str, int]], abundances_vector: List[float]) -> Optional[List[List[float]]]:
+############################ rps_for_cell_lines ####################################
+def rps_for_cell_lines(dataframe: pd.DataFrame, reactions: Dict[str, dict[str, int]], black_list: List[str], syn_dict: Dict[str, List[str]]) -> List[Dict[str, float]]:
     """
-    Calculate RPS scores for each reaction based on the provided reactions and updated abundances.
+    Calculate Reaction Propensity Scores (RPS) for each cell line represented in the dataframe.
 
-    Args:
-        reactions (dict): A dictionary containing reactions data.
-                          Keys are reaction IDs or indices, and values are dictionaries representing stoichiometric coefficients.
-        abundances_vector (list): A list containing updated metabolite abundances.
+    Parameters:
+        dataframe (pandas.DataFrame): A DataFrame containing metabolite abundances for different cell lines.
+        reactions (dict): A dictionary representing reactions where keys are reaction names and values are dictionaries containing metabolite names as keys and stoichiometric coefficients as values.
+        black_list (list): A list containing metabolite names that should be excluded from the RPS calculation.
+        syn_dict (dict): A dictionary where keys are general metabolite names and values are lists of possible synonyms.
 
     Returns:
-        list: A list of RPS scores for each reaction.
-    """
-    rps_scores = []
-
-    for reaction_id, stoichiometry in reactions.items():
-        coeff_vector = extract_coeff_vector(reactions, reaction_id)
-
-        if coeff_vector is not None:
-            rps_score = rps_calculator(coeff_vector, abundances_vector)
-            rps_scores.append(rps_score)
-        else:
-            rps_scores.append(None)
-
-    return rps_scores
-############################ MAIN #############################################
-def main() -> None:
-    """
-    Initializes everything and sets the program in motion based on the fronted input arguments.
-
-    Returns:
-        None
-    """
+        list[dict]: A list of dictionaries containing Reaction Propensity Scores (RPS) for each cell line. Each dictionary contains reaction names as keys and their corresponding RPS scores as values.
     
+    """
+    rps_scores=[]
+    for series in dataframe.rows():
+     updated_abundances = update_metabolite_names(series.to_dict(), syn_dict)
+     abundances, missing_list = check_missing_metab(reactions, updated_abundances)
+     rps_scores.append(calculate_rps(reactions, abundances, black_list, missing_list))
+    return rps_scores
+
+
+############################ main ####################################
