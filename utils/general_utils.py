3d363be8d128b0e7513487b0e8990bb712d3e362
Francesco2304 <f.lapi@campus.unimib.it>
2025-09-05 15:02:24 +0200

    add gene manipulation

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -17,6 +17,8 @@ import gzip
 import bz2
 from io import StringIO
 
+
+
 class ValueErr(Exception):
     def __init__(self, param_name, expected, actual):
         super().__init__(f"Invalid value for {param_name}: expected {expected}, got {actual}")
@@ -699,3 +701,16 @@ class Model(Enum):
 
 
     def __str__(self) -> str: return self.value
+
+
+def convert_genes(model,annotation):
+    from cobra.manipulation import rename_genes
+    model2=model.copy()
+    try:
+        dict_genes={gene.id:gene.notes[annotation]  for gene in model2.genes}
+    except:
+        print("No annotation in gene dict!")
+        return -1
+    rename_genes(model2,dict_genes)
+
+    return model2
